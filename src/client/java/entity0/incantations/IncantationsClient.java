e8dc0a872afb25fbeb8df67d145c421480ff221f
Ilikepinapples <willschofield08@gmail.com>
2024-08-27 00:41:15 +0100

    Initial commit

---
+++ src/client/java/entity0/incantations/IncantationsClient.java
@@ -0,0 +1,10 @@
+package entity0.incantations;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IncantationsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
